6461c6b57f473dc2b6ebe27e29dfcc16ef803dc8
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-03-02 12:51:39 +0530

    Update product fetch URL to production endpoint

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try{
-      const url = "http://localhost:8000/products"; 
+      const url = "https://implementedlsback.vercel.app/products"; 
       const headers = {
         headers: {
           'Authorization': localStorage.getItem('token') 
